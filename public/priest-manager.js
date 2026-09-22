dc2ecc2a105ee27bc5089d2c2ed9a516a0a7354d
Nguyễn Anh Tân <joseph.nguyenanhtan@outlook.com>
2026-09-22 21:26:55 +0700

    fdssdgsdgh

--- public/priest-manager.js
+++ public/priest-manager.js
@@ -51,7 +51,7 @@ async function importPriestFromDocx(file) {
   });
 
   if (!response.ok) {
-    throw new Error(await readErrorMessage(response, 'Failed to import priest profile from DX'+response));
+    throw new Error(await readErrorMessage(response, 'Failed to import priest profile from DX'));
   }
 
   return response.json();
